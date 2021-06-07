c5e3a63de6726ac6ec3165eadc1ce112ca8b1d7a
Moch Faizal Ansyori <faizmuhammad810@gmail.com>
2021-06-07 15:09:07 +0700

    api workshop

--- api/controller/TaskController.js
+++ api/controller/TaskController.js
@@ -1,23 +1,53 @@
+const knex = require('../../db/knex');
 
 exports.get_task = async function(req, res, next) {
     try{        
-        const cek_user = await knex.raw(`
-            select * from tasks t 
-        `);
-        console.log(cek_user)
-        // const jumlah_data = parseInt(cek_user.rows.length)
-        // if(jumlah_data==0){
-        //     res.status(200).json({
-        //         success:false,
-        //         message:"Data tidak ditemukan",
-        //     });
-        // }else{
-        //     res.status(200).json({
-        //         success:true,
-        //         message:"Data ditemukan",
-        //         data:cek_user.rows,
-        //     });
-        // }
+        let query = `select * from tasks t `
+        if(req.query.keyword != ''){
+            query += `where t.task_name LIKE '%${req.query.keyword}%'`
+        }
+        const cek_user = await knex.raw(query);
+        const jumlah_data = parseInt(cek_user[0].length)
+        if(jumlah_data==0){
+            res.status(200).json({
+                success:false,
+                message:"Data tidak ditemukan",
+                data: cek_user[0]
+            });
+        }else{
+            res.status(200).json({
+                success:true,
+                message:"Data ditemukan",
+                data:cek_user[0],
+            });
+        }
+    } catch (err){
+        console.log(err)
+        return res.status(500).json({ msg: 'Internal server error' });
+    }
+};
+
+exports.submit = async function(req, res, next) {
+    try{      
+        const id = req.body.id; 
+        const hasil = req.body.hasil; 
+
+        let query = `
+        update tasks set hasil = '${hasil}',  is_done = true, time_finished = now()
+        where id = ${id}`
+        await knex.raw(query)
+        .then((x) => {
+            res.json({
+                success:true,
+                message:"Berhasil memperbarui data"
+            })
+        })
+        .catch((err) => {
+            res.status(500).json({
+                success:false,
+                message:"Terjadi kesalahan"
+            })
+        })
     } catch (err){
         console.log(err)
         return res.status(500).json({ msg: 'Internal server error' });
--- routes/modules/taskRoutes.js
+++ routes/modules/taskRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const task_api = require("../../api/controller/TaskController");
 const router = express.Router();
-router.get('/task', task_api.get_task);
+router.get('/', task_api.get_task);
+router.post('/submit', task_api.submit);
 
 module.exports = router;
